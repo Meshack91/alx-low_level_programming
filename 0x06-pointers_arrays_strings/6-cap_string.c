695f3f0794c768339587148da454f6e202d6f912
Meshack91 <petermeshackwaate35@gmail.com>
2023-09-20 16:11:07 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: the string to be capitalized
+ * Return: a pointer
+ */
+char *cap_string(char *)
+{
+int index = 0;
+
+while (str[index])
