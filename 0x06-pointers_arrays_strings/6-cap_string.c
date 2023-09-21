65c0bd43707ae5e0403738ef4d3e5e6d656915bd
Meshack91 <petermeshackwaate35@gmail.com>
2023-09-21 11:47:38 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,28 @@
 #include "main.h"
 /**
  * cap_string - capitalizes all words of a string
- * @str: the string to be capitalized
- * Return: a pointer
+ * @str: input string.
+ * Return: the pointer to dest.
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-int index = 0;
+int i = 0;
 
-while (str[index])
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+					str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+					str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+					str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+					str[i - 1] == '}')
+{
+				str[i] -= 32;
+}
+}
+i++;
+}
+
+return (str);
+}
