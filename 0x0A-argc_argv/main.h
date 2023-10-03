a33d7f9b05b85495de75468d04d38df1db924902
Meshack91 <petermeshackwaate35@gmail.com>
2023-10-03 12:02:41 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]__attribute__((unused)));
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int _putchar(char c);
+#endif
